60ecf5428cc508c65231f0a5d11316adc966e4f6
Chimezie Nwagbo <chimezienwagbotech@gmail.com>
2025-11-06 08:09:06 +0100

    feature: sidebar menu ui

--- src/layout/appLayout/AppLayout.tsx
+++ src/layout/appLayout/AppLayout.tsx
@@ -1,7 +1,7 @@
-import React from "react";
 import { Outlet } from "react-router-dom";
 import Navbar from "../navbar/Navbar";
-import Sidebar from "../sidebar/Sidebar";
+
+import ResponsiveSidebar from "../sidebar/responsiveSidebar/ResponsiveSidebar";
 import "./AppLayout.scss";
 
 const AppLayout = () => {
@@ -9,7 +9,7 @@ const AppLayout = () => {
     <div className="app-layout">
       <Navbar />
       <div className="app-layout__body">
-        <Sidebar />
+        <ResponsiveSidebar />
         <main className="app-layout__content">
           <Outlet /> {/* This is where child pages render */}
         </main>
---
+++ src/pages/notAvailable/NotAvailable.tsx
@@ -0,0 +1,12 @@
+import "./NotAvailable.scss";
+
+const NotAvailable = () => {
+  return (
+    <div className="feature-not-available">
+      <h2>🚧 Feature Not Available</h2>
+      <p>This feature is coming soon. Please check back later!</p>
+    </div>
+  );
+};
+
+export default NotAvailable;
